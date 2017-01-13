37bc69e160321ac8deaf213636b05fb885e69ed2
Alexey Orlenko <eaglexrlnk@gmail.com>
2017-01-13 15:46:12 +0200

    test: simplify line-case.test.js

--- test/rules/line-case.test.js
+++ test/rules/line-case.test.js
@@ -4,56 +4,38 @@ const assert = require('assert');
 const parser = require('../../lib/parser');
 const rules = require('../../lib/rules');
 
-const LOWERCASE_MSG_NOTAG = `\
-implement a feature
-`;
-
-const UPPERCASE_MSG_NOTAG = `\
-Implement a feature
-`;
-
-const LOWERCASE_MSG_TAG = `\
-subsystem: implement a feature
-`;
-
-const UPPERCASE_MSG_TAG = `\
-subsystem: Implement a feature
-`;
-
-const LOWERCASE_MSG_UPPERCASE_TAG = `\
-Subsystem: implement a feature
-`;
-
-const UPPERCASE_MSG_UPPERCASE_TAG = `\
-Subsystem: Implement a feature
-`;
-
 exports.testLowercaseMsgNoTag = () => {
-  const errors = rules.lintCase(parser.parse(LOWERCASE_MSG_NOTAG));
+  const message = parser.parse('implement a feature');
+  const errors = rules.lintCase(message);
   assert.strictEqual(errors.length, 1);
 };
 
 exports.testUppercaseMsgNoTag = () => {
-  const errors = rules.lintCase(parser.parse(UPPERCASE_MSG_NOTAG));
+  const message = parser.parse('Implement a feature');
+  const errors = rules.lintCase(message);
   assert.strictEqual(errors.length, 0);
 };
 
 exports.testLowercaseMsgTag = () => {
-  const errors = rules.lintCase(parser.parse(LOWERCASE_MSG_TAG));
+  const message = parser.parse('subsystem: implement a feature');
+  const errors = rules.lintCase(message);
   assert.strictEqual(errors.length, 0);
 };
 
 exports.testUppercaseMsgTag = () => {
-  const errors = rules.lintCase(parser.parse(UPPERCASE_MSG_TAG));
+  const message = parser.parse('subsystem: Implement a feature');
+  const errors = rules.lintCase(message);
   assert.strictEqual(errors.length, 1);
 };
 
 exports.testLowercaseMsgUppercaseTag = () => {
-  const errors = rules.lintCase(parser.parse(LOWERCASE_MSG_UPPERCASE_TAG));
+  const message = parser.parse('Subsystem: implement a feature');
+  const errors = rules.lintCase(message);
   assert.strictEqual(errors.length, 1);
 };
 
 exports.testUppercaseMsgUppercaseTag = () => {
-  const errors = rules.lintCase(parser.parse(UPPERCASE_MSG_UPPERCASE_TAG));
+  const message = parser.parse('Subsystem: Implement a feature');
+  const errors = rules.lintCase(message);
   assert.strictEqual(errors.length, 2);
 };
